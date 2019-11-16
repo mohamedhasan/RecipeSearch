4f1b5813dbae47bd8dd767c924ce2b8240e6e50d
Mohamed Hassan <mohamedhasan_17@hotmail.com>
2019-11-16 16:01:44 +0200

    Project settings

---
+++ RecipeSearch/ViewControllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeSearch
+//
+//  Created by Mohamed Hassan on 11/16/19.
+//  Copyright © 2019 Mohamed Hassan. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.titleView = UIImageView(image: UIImage(named: "logo"))
+    }
+
+
+}
+
